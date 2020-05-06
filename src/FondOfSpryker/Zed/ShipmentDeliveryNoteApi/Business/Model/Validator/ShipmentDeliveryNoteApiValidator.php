70ab5b7d8587b4b040437bd803f7724f962aec7d
Daniel Rose <daniel-rose@gmx.de>
2020-05-06 20:25:09 +0200

    - remove unused method - add required dependencies to composer.json

--- src/FondOfSpryker/Zed/ShipmentDeliveryNoteApi/Business/Model/Validator/ShipmentDeliveryNoteApiValidator.php
+++ src/FondOfSpryker/Zed/ShipmentDeliveryNoteApi/Business/Model/Validator/ShipmentDeliveryNoteApiValidator.php
@@ -14,28 +14,5 @@ class ShipmentDeliveryNoteApiValidator implements ShipmentDeliveryNoteApiValidat
     public function validate(ApiDataTransfer $apiDataTransfer): array
     {
         return [];
-        /*$data = $apiDataTransfer->getData();
-
-        $errors = [];
-        //$errors = $this->assertRequiredField($data, '', $errors);
-
-        return $errors;*/
-    }
-
-    /**
-     * @param array $data
-     * @param string $field
-     * @param array $errors
-     *
-     * @return array
-     */
-    protected function assertRequiredField(array $data, string $field, array $errors): array
-    {
-        if (!isset($data[$field]) || !array_key_exists($field, $data)) {
-            $message = sprintf('Missing value for required field "%s"', $field);
-            $errors[$field][] = $message;
-        }
-
-        return $errors;
     }
 }
